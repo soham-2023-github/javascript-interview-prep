46329b372e67739b0447d307e173fd6865cf13c7
soham-2023-github <135940278+soham-2023-github@users.noreply.github.com>
2023-06-11 21:13:10 -0400

    Update palindrome-check.js

--- palindrome-check.js
+++ palindrome-check.js
@@ -1,11 +1,14 @@
 // Palindrome Check: Write a function to determine whether a given string is a palindrome.
 
-function reverseString(str) {
-    str.split('').reverse().join();
- }
-
 function checkPalindrome(str) {
-  return reverseString(str).toLowerCase() === str.toLowerCase();
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i].toLowerCase() !== str[len - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
 }
 
-console.log(checkPalindrome('bob'));
+console.log(checkPalindrome('bob')); // Output: true
+
