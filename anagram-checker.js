e4865c9ee5766c94b3565a92e8ba016a2470bb6b
soham-2023-github <135940278+soham-2023-github@users.noreply.github.com>
2023-06-13 00:21:09 -0400

    Update anagram-checker.js

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,5 +1,23 @@
 // Anagram Check: Write a function to determine if two strings are anagrams of each other.
 
-function anagramCheck (str1, str2) {
-  return str1.split('').sort().join() === str2.split('').sort().join()
+function anagramCheck(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const charCount = {};
+
+  for (let char of str1) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+
+  for (let char of str2) {
+    if (!charCount[char]) {
+      return false;
+    }
+    charCount[char]--;
+  }
+
+  return true;
 }
+
